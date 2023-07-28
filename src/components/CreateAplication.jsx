9302b0b8316657012a3479d7f7c63f9dddc40d3c
Luis Adan Gomez Torres <lgomez1290@gmail.com>
2023-07-27 18:51:00 -0600

    Inicianco con agregar aplicacion

--- src/components/CreateAplication.jsx
+++ src/components/CreateAplication.jsx
@@ -23,6 +23,7 @@ import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCompaniesThunk } from '../store/slices/companies.slice';
+import axios from 'axios';
 
 function Copyright(props) {
   return (
@@ -50,6 +51,8 @@ const CreateAplication = ({ themeGlobal }) => {
   const [company, setCompany] = useState('');
   const [selectedDate, setSelectedDate] = useState(null);
 
+
+
   useEffect(() => {
     dispatch(getCompaniesThunk())
   }, [company, selectedDate]);
@@ -69,27 +72,57 @@ const CreateAplication = ({ themeGlobal }) => {
 
   const [formData, setFormData] = useState({});
 
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log('datos pusos sin el estado', {
+  //     name: data.get('nameAplication'),
+  //     description: data.get('description'),
+  //     date_share: selectedDate,
+  //     uer_id: parseInt(id, 10),
+  //     company_id: company
+  //   });
+
+  //   setFormData({
+  //     name: data.get('nameAplication'),
+  //     description: data.get('description'),
+  //     date_share: selectedDate,
+  //     uer_id: parseInt(id, 10),
+  //     company_id: company
+  //   })
+
+  //   console.log(formData)
+  // };
+
+
   const handleSubmit = (event) => {
+
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log('datos pusos sin el estado', {
-      name: data.get('nameAplication'),
-      description: data.get('description'),
-      date_share: selectedDate,
-      uer_id: parseInt(id, 10),
-      company_id: company
-    });
+     const data = new FormData(event.currentTarget);
+    const { nameAplication, description, date } = event.target;
+    const name = nameAplication.value
+    const descriptionApli = description.value
+    const dateApli = date.value
+
 
     setFormData({
       name: data.get('nameAplication'),
       description: data.get('description'),
-      date_share: selectedDate,
+      date_share: data.get('date'),
       uer_id: parseInt(id, 10),
       company_id: company
     })
-
     console.log(formData)
-  };
+
+
+    axios.post('http://localhost:8000/aplicationjob', { name, descriptionApli, dateApli, id, company })
+      .then((res) => {
+        console.log(res)
+        navigate("/")
+      })
+      .catch((error) => console.error(error));
+  }
+
   const [open, setOpen] = useState(true);
 
   const handleClick = () => {
@@ -131,24 +164,34 @@ const CreateAplication = ({ themeGlobal }) => {
                 />
               </Grid>
               <Grid item xs={11} sm={11} md={11}>
-                <TextareaAutosize
-                  className='textArea'
-                  name="description"
-                  aria-label="empty textarea"
-                  placeholder="Descripción"
+
+                <TextField
+                  required
                   id="description"
-                  color="inherit"
+                  name="description"
+                  label="Descripción"
+                  multiline
+                  rows={4}
+                  autoFocus
+                  fullWidth
                 />
               </Grid>
               <Grid item xs={11} sm={11} md={11}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DatePicker
                     label="Fecha"
+                    type="date"
                     value={selectedDate}
                     onChange={handleDateChange}
                     renderInput={(params) => <TextField {...params} />}
                   />
-                </LocalizationProvider>
+                </LocalizationProvider> */}
+                <TextField
+                  id="date"
+                  label="fecha"
+                  name="fecha"
+                  type="date"
+                />
               </Grid>
 
               <Grid item xs={11} sm={11} md={11}>
