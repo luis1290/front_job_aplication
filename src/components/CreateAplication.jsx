db6d8feb0ea9d0d1f5d7089c0c212e10554620f6
Luis Adan Gomez Torres <lgomez1290@gmail.com>
2023-07-28 09:49:37 -0600

    abance de formulario de creacion de aplicacion

--- src/components/CreateAplication.jsx
+++ src/components/CreateAplication.jsx
@@ -24,6 +24,7 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCompaniesThunk } from '../store/slices/companies.slice';
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
 
 function Copyright(props) {
   return (
@@ -51,6 +52,9 @@ const CreateAplication = ({ themeGlobal }) => {
   const [company, setCompany] = useState('');
   const [selectedDate, setSelectedDate] = useState(null);
 
+  // const { register, handleSubmit, reset } = useForm();
+
+
 
 
   useEffect(() => {
@@ -66,62 +70,38 @@ const CreateAplication = ({ themeGlobal }) => {
 
 
 
-  const handleChange = (event) => {
-    setCompany(event.target.value);
-  };
-
+  
   const [formData, setFormData] = useState({});
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   console.log('datos pusos sin el estado', {
-  //     name: data.get('nameAplication'),
-  //     description: data.get('description'),
-  //     date_share: selectedDate,
-  //     uer_id: parseInt(id, 10),
-  //     company_id: company
-  //   });
+  
 
-  //   setFormData({
-  //     name: data.get('nameAplication'),
-  //     description: data.get('description'),
-  //     date_share: selectedDate,
-  //     uer_id: parseInt(id, 10),
-  //     company_id: company
-  //   })
+  const [formValues, setFormValues] = useState({
+    nameAplication: '',
+    description: '',
+    fecha: '',
+    company: '',
+    user_id: id
+  });
 
-  //   console.log(formData)
-  // };
-
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (event) => {
-
     event.preventDefault();
-     const data = new FormData(event.currentTarget);
-    const { nameAplication, description, date } = event.target;
-    const name = nameAplication.value
-    const descriptionApli = description.value
-    const dateApli = date.value
-
-
-    setFormData({
-      name: data.get('nameAplication'),
-      description: data.get('description'),
-      date_share: data.get('date'),
-      uer_id: parseInt(id, 10),
-      company_id: company
-    })
-    console.log(formData)
-
-
-    axios.post('http://localhost:8000/aplicationjob', { name, descriptionApli, dateApli, id, company })
-      .then((res) => {
-        console.log(res)
-        navigate("/")
-      })
-      .catch((error) => console.error(error));
-  }
+    // Aquí puedes manejar la lógica para enviar los datos del formulario
+    //   axios.post('http://localhost:8000/aplicationjob', { name, descriptionApli, dateApli, id, company })
+    //     .then((res) => {
+    //       console.log(res)
+    //       navigate("/")
+    //     })
+    //     .catch((error) => console.error(error));
+    console.log('Valores del formulario:', formValues);
+  };
 
   const [open, setOpen] = useState(true);
 
@@ -129,6 +109,9 @@ const CreateAplication = ({ themeGlobal }) => {
     setOpen(!open);
   };
 
+  const submit = data => {
+    console.log(data)
+  }
 
 
 
@@ -150,17 +133,19 @@ const CreateAplication = ({ themeGlobal }) => {
           <Typography component="h1" variant="h5">
             Crear una aplicación
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <form onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={11} sm={11} md={11}>
                 <TextField
                   autoComplete="given-name"
                   name="nameAplication"
+                  value={formValues.nameAplication}
                   required
                   fullWidth
                   id="nameAplication"
                   label="Nombre Aplicación"
                   autoFocus
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={11} sm={11} md={11}>
@@ -170,6 +155,8 @@ const CreateAplication = ({ themeGlobal }) => {
                   id="description"
                   name="description"
                   label="Descripción"
+                  value={formValues.description}
+                  onChange={handleChange}
                   multiline
                   rows={4}
                   autoFocus
@@ -177,29 +164,24 @@ const CreateAplication = ({ themeGlobal }) => {
                 />
               </Grid>
               <Grid item xs={11} sm={11} md={11}>
-                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Fecha"
-                    type="date"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider> */}
+
                 <TextField
                   id="date"
                   label="fecha"
                   name="fecha"
                   type="date"
+                  value={formValues.fecha}
+                  onChange={handleChange}
                 />
               </Grid>
 
               <Grid item xs={11} sm={11} md={11}>
                 <InputLabel id="demo-simple-select-label">Empresas</InputLabel>
                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={company}
+                  labelId="company"
+                  id="company"
+                  name="company"
+                  value={formValues.company}
                   label="Age"
                   onChange={handleChange}
                 >
@@ -217,7 +199,7 @@ const CreateAplication = ({ themeGlobal }) => {
             >
               Agregar aplicación
             </Button>
-          </Box>
+          </form>
         </Box>
       </Container>
     </ThemeProvider>
